d701a1e72666e454a4d0891eafd9472f980c47e7
sakibrimon <mdsakibhassanrimon@gmail.com>
2025-05-26 23:46:20 +0600

    commented unnecessary queries

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -47,9 +47,9 @@ INSERT INTO sightings (ranger_id, species_id, sighting_time, location, notes) VA
 (3, 3, '2024-05-15 09:10:00', 'Bamboo Grove East', 'Feeding observed'),
 (2, 1, '2024-05-18 18:30:00', 'Snowfall Pass', NULL);
 
-SELECT * FROM rangers;
-SELECT * FROM species;
-SELECT * FROM sightings;
+-- SELECT * FROM rangers;
+-- SELECT * FROM species;
+-- SELECT * FROM sightings;
 
 -- DROP TABLE rangers;
 -- DROP TABLE species;
